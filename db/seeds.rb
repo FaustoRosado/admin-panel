50edff55166c42f0282de5bb96f0b5565188f649
Fausto Rosado <faustrosado@gmail.com>
2019-02-06 08:35:28 -0500

    updated student data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+education = ["Highschool Diploma", "Bachlor Degree", "Master's Degree", "Ph.D"]
+
 100.times do |i|
   Student.create!(
     first_name: Faker::Name.first_name,
      last_name: Faker::Name.last_name,
       email: Faker::Internet.user_name + i.to_s + "@#{Faker::Internet.domain_name}"), 
         username: "#{Faker::Internet.user_name}#{i}",
-          age: Faker::Number.rand(1...150)
+          age: Faker::Number.number(2)
             birthday: Faker::Date:birthday
+              education: "#{education[rand(1..3]}"
+                  )
          
-  print '.' if i % 1000 == 0
+  # print '.' if i % 1000 == 0
 end
-puts
+# puts
