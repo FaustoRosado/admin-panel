dd0775ff75aab72c235e7546c591448fb59f09ce
Fausto Rosado <faustrosado@gmail.com>
2019-02-05 23:33:11 -0500

    created student migration and student model

---
+++ db/migrate/20190206043202_create_students.rb
@@ -0,0 +1,15 @@
+class CreateStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :username
+      t.integer :age
+      t.date :birthday
+      t.string :education
+
+      t.timestamps
+    end
+  end
+end
